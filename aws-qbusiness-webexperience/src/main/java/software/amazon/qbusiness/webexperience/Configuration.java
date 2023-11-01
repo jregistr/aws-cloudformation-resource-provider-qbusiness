9f652d4bc647915fe4d34d2d669ecfbda75d2a32
Jeff Registre <jeregist@amazon.com>
2023-11-01 13:21:08 -0400

    Used cfn cli to generate boilerplate handler files for WebExperience.

---
+++ aws-qbusiness-webexperience/src/main/java/software/amazon/qbusiness/webexperience/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.qbusiness.webexperience;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-qbusiness-webexperience.json");
+    }
+}
