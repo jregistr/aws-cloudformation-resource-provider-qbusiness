4ccddcb435750f67446ab7fb1c880bc05b28b539
Jeff Registre <jeregist@amazon.com>
2023-11-01 13:11:39 -0400

    Generated boilerplate handler files for retriever using the cfn cli's init command.

---
+++ aws-qbusiness-retriever/src/main/java/software/amazon/qbusiness/retriever/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.qbusiness.retriever;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-qbusiness-retriever.json");
+    }
+}
