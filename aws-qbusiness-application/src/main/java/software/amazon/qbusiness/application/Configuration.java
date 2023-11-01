76bac65ecd2cdbcc8e1f2a3b9de477d7eba52a5a
Jeff Registre <jeregist@amazon.com>
2023-10-31 23:06:10 -0400

    Generated files for Application resource using cfn init.

---
+++ aws-qbusiness-application/src/main/java/software/amazon/qbusiness/application/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.qbusiness.application;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-qbusiness-application.json");
+    }
+}
