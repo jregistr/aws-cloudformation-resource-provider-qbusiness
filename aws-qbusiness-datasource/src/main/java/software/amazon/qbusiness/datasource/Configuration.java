f5fa4f66dbed700ab3d40ec39c29b734bf23047b
Jeff Registre <jeregist@amazon.com>
2023-11-01 13:00:31 -0400

    Generated boilerplate handler package for DataSource.

---
+++ aws-qbusiness-datasource/src/main/java/software/amazon/qbusiness/datasource/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.qbusiness.datasource;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-qbusiness-datasource.json");
+    }
+}
