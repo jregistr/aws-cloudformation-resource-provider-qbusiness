8d5c830c33f514baaff98bf13cb93005cdef0325
Jeff Registre <jeregist@amazon.com>
2023-11-08 18:46:28 -0500

    Override endpoint url for Client. (#5)
    Overriding the client endpoint url to correct UnknownHostException.

--- aws-qbusiness-application/src/main/java/software/amazon/qbusiness/application/ClientBuilder.java
+++ aws-qbusiness-application/src/main/java/software/amazon/qbusiness/application/ClientBuilder.java
@@ -1,25 +1,25 @@
 package software.amazon.qbusiness.application;
 
-import software.amazon.awssdk.core.SdkClient;
-// TODO: replace all usage of SdkClient with your service client type, e.g; YourServiceClient
-// import software.amazon.awssdk.services.yourservice.YourServiceClient;
-// import software.amazon.cloudformation.LambdaWrapper;
+import static software.amazon.qbusiness.application.Constants.ENV_AWS_REGION;
+import static software.amazon.qbusiness.application.Constants.SERVICE_NAME_LOWER;
+
+import java.net.URI;
+
+import software.amazon.awssdk.services.qbusiness.QBusinessClient;
+import software.amazon.cloudformation.LambdaWrapper;
 
 public class ClientBuilder {
-  /*
-  TODO: uncomment the following, replacing YourServiceClient with your service client name
-  It is recommended to use static HTTP client so less memory is consumed
-  e.g. https://github.com/aws-cloudformation/aws-cloudformation-resource-providers-logs/blob/master/aws-logs-loggroup/src/main/java/software/amazon/logs/loggroup/ClientBuilder.java#L9
 
-  public static YourServiceClient getClient() {
-    return YourServiceClient.builder()
-              .httpClient(LambdaWrapper.HTTP_CLIENT)
-              .build();
-  }
-  */
+  // {service}.{region}.api.aws
+  private static final String URL_PATTERN = "%s.%s.api.aws";
 
-  // TODO: remove this implementation once you have uncommented the above
-  public static SdkClient getClient() {
-    return null;
+  public static QBusinessClient getClient() {
+    String region = System.getenv(ENV_AWS_REGION);
+    var urlString = URL_PATTERN.formatted(SERVICE_NAME_LOWER, region);
+
+    return QBusinessClient.builder()
+        .httpClient(LambdaWrapper.HTTP_CLIENT)
+        .endpointOverride(URI.create(urlString))
+        .build();
   }
 }
--- aws-qbusiness-application/src/main/java/software/amazon/qbusiness/application/Constants.java
+++ aws-qbusiness-application/src/main/java/software/amazon/qbusiness/application/Constants.java
@@ -9,6 +9,7 @@ public final class Constants {
   public static final String API_DELETE_APPLICATION = "DeleteApplication";
   public static final String API_UPDATE_APPLICATION = "UpdateApplication";
   public static final String SERVICE_NAME_LOWER = SERVICE_NAME.toLowerCase(Locale.ENGLISH);
+  public static final String ENV_AWS_REGION = "AWS_REGION";
 
   private Constants() {
   }
